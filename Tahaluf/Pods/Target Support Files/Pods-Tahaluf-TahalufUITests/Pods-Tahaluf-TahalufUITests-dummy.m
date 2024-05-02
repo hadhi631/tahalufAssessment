61e7b566b6cd249883b487421e848aea7056b3b6
Hadhi <hadhi631@gmail.com>
2024-05-02 08:55:06 +0400

    Did initial project setup with Cocoapods

---
+++ Tahaluf/Pods/Target Support Files/Pods-Tahaluf-TahalufUITests/Pods-Tahaluf-TahalufUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tahaluf_TahalufUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tahaluf_TahalufUITests
+@end
---
+++ Tahaluf/Pods/Target Support Files/Pods-Tahaluf/Pods-Tahaluf-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tahaluf : NSObject
+@end
+@implementation PodsDummy_Pods_Tahaluf
+@end
